17097c39a41bf692a599ce78359dc06d126ffe52
Gustavo Giroldo <gustavoglu@hotmail.com>
2017-08-09 21:38:26 -0300

    Trabalhando com o Identity

---
+++ src/Events.Infra.Identity/Data/UsuarioContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Events.Site.Models;
+
+namespace Events.Infra.Identity.Data
+{
+    public class UsuarioContext : IdentityDbContext<Usuario>
+    {
+        public UsuarioContext(DbContextOptions<UsuarioContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
